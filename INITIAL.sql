11f460894aefea0cfd56a4aad5988847da7b7959
gorsheninasvetlana <gorshenina.svetlana@gmail.com>
2024-10-28 20:26:44 +0300

    Update INITIAL.sql

--- INITIAL.sql
+++ INITIAL.sql
@@ -1,1 +1,13 @@
-/*сохраните в этом файле запросы для первоначальной загрузки данных - создание схемы raw_data и таблицы sales и наполнение их данными из csv файла*/
+CREATE SCHEMA IF NOT EXISTS raw_data;
+create table if not exists raw_data.sales(
+id integer,
+auto text,
+gasoline_consumption text,
+price numeric(9,2),
+date date,
+person_name text,
+phone text,
+discount integer,
+brand_origin text);
+
+/copy raw_data.sales from '/Users/cars.csv' with csv header null 'null'
