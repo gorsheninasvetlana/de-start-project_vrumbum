6e1f502974328d18eacde677ac6da073ccddd9a8
gorsheninasvetlana <gorshenina.svetlana@gmail.com>
2024-10-29 21:45:56 +0300

    Update INITIAL.sql

--- INITIAL.sql
+++ INITIAL.sql
@@ -1,13 +1,15 @@
+-- Создаем схему raw_data
 CREATE SCHEMA IF NOT EXISTS raw_data;
-create table if not exists raw_data.sales(
-id integer,
-auto text,
-gasoline_consumption text,
-price numeric(9,2),
-date date,
-person_name text,
-phone text,
-discount integer,
-brand_origin text);
 
-/copy raw_data.sales from '/Users/cars.csv' with csv header null 'null'
+-- Создаем таблицу sales в схеме raw_data
+CREATE TABLE IF NOT EXISTS raw_data.sales (
+    id INTEGER,
+    auto TEXT, 
+    gasoline_consumption TEXT,
+    price NUMERIC(9, 2),
+    date DATE,
+    person_name TEXT,
+    phone TEXT,
+    discount INTEGER,
+    brand_origin TEXT
+);
