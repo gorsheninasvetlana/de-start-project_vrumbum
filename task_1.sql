b98df85078b9bd4ecd72cac5b5117c43eea8a840
gorsheninasvetlana <gorshenina.svetlana@gmail.com>
2024-10-30 22:59:31 +0300

    Update task_1.sql

--- task_1.sql
+++ task_1.sql
@@ -1,1 +1,5 @@
 /*добавьте сюда запрос для решения задания 1*/
+SELECT 
+    (COUNT(model_id) - COUNT(gasoline_consumption)) * 100.0 / COUNT(model_id) AS nulls_percentage_gasoline_consumption
+FROM 
+    car_shop.model;
